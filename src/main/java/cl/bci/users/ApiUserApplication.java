efd2db8dc9f8649638cdba3eab08dae98e308262
Williams Gomez <williams.gomez@imagemaker.com>
2021-10-19 00:59:30 -0300

    feat: se habilita swagger

--- src/main/java/cl/bci/users/ApiUserApplication.java
+++ src/main/java/cl/bci/users/ApiUserApplication.java
@@ -2,34 +2,12 @@ package cl.bci.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import cl.bci.users.util.JWTAuthorizationFilter;
 
 @SpringBootApplication
 public class ApiUserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiUserApplication.class, args);
-	}
-	
-	@EnableWebSecurity
-	@Configuration
-	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable()
-				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
-				.authorizeRequests()
-				.antMatchers(HttpMethod.POST, "/login").permitAll()
-				.anyRequest().authenticated();
-		}
 	}
 
 }
---
+++ src/main/java/cl/bci/users/config/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package cl.bci.users.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import cl.bci.users.util.JWTAuthorizationFilter;
+
+@EnableWebSecurity
+@Configuration
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+			.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+			.authorizeRequests()
+			.antMatchers(HttpMethod.POST, "/login").permitAll()
+			.antMatchers("/v2/api-docs", "/swagger-resources/configuration/ui", "/swagger-resources", "/swagger-resources/configuration/security", "/swagger-ui.html", "/webjars/**").permitAll()
+			.anyRequest().authenticated()
+			;
+	}
+	
+
+	
+}
